c62fc673b4b1e0e54e8dd11066eb59cee6cb15ef
Karl Murray <karlm89@me.com>
2020-11-04 02:01:51 -0600

    Finished episode 4

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -5,5 +5,108 @@
 @endsection
 
 @section('content')
-    <h1>Game Title</h1>
+    <div class="container mx-auto px-4">
+        <div class="game-details border-b border-gray-800 pb-12 flex">
+            <div class="flex-none">
+                <img src="/assets/resident.jpg" alt="cover image">
+            </div>
+            <div class="ml-12">
+                <h1 class="font-semibold text-4xl">Resident Evil 3</h1>
+                <div class="text-gray-400">
+                    <span>Survival Horror</span>
+                    &middot;
+                    <span>Capcom</span>
+                    &middot;
+                    <span>PS4</span>
+                </div>
+                <div class="flex flex-wrap items-center mt-8">
+                    <div class="flex items-center">
+                        <div class="w-16 h-16 bg-gray-800 rounded-full">
+                            <div class="font-semibold text-xs flex justify-center items-center h-full">90%</div>
+                        </div>
+                        <div class="ml-4 text-xs">Member <br> Score</div>
+                    </div>
+                    <div class="flex items-center ml-12">
+                        <div class="w-16 h-16 bg-gray-800 rounded-full">
+                            <div class="font-semibold text-xs flex justify-center items-center h-full">90%</div>
+                        </div>
+                        <div class="ml-4 text-xs">Critic <br> Score</div>
+                    </div>
+                    
+                    <div class="flex items-center space-x-4 ml-12"><!-- social -->
+
+                        <div class="w-8 h-8 bg-gray-800 rounded-full flex justify-center items-center"><!-- web -->
+                            <a href="#" class="hover:text-gray-400">
+                                <svg class="fill-current w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path d="M248 8C111.03 8 0 119.03 0 256s111.03 248 248 248 248-111.03 248-248S384.97 8 248 8zm82.29 357.6c-3.9 3.88-7.99 7.95-11.31 11.28-2.99 3-5.1 6.7-6.17 10.71-1.51 5.66-2.73 11.38-4.77 16.87l-17.39 46.85c-13.76 3-28 4.69-42.65 4.69v-27.38c1.69-12.62-7.64-36.26-22.63-51.25-6-6-9.37-14.14-9.37-22.63v-32.01c0-11.64-6.27-22.34-16.46-27.97-14.37-7.95-34.81-19.06-48.81-26.11-11.48-5.78-22.1-13.14-31.65-21.75l-.8-.72a114.792 114.792 0 0 1-18.06-20.74c-9.38-13.77-24.66-36.42-34.59-51.14 20.47-45.5 57.36-82.04 103.2-101.89l24.01 12.01C203.48 89.74 216 82.01 216 70.11v-11.3c7.99-1.29 16.12-2.11 24.39-2.42l28.3 28.3c6.25 6.25 6.25 16.38 0 22.63L264 112l-10.34 10.34c-3.12 3.12-3.12 8.19 0 11.31l4.69 4.69c3.12 3.12 3.12 8.19 0 11.31l-8 8a8.008 8.008 0 0 1-5.66 2.34h-8.99c-2.08 0-4.08.81-5.58 2.27l-9.92 9.65a8.008 8.008 0 0 0-1.58 9.31l15.59 31.19c2.66 5.32-1.21 11.58-7.15 11.58h-5.64c-1.93 0-3.79-.7-5.24-1.96l-9.28-8.06a16.017 16.017 0 0 0-15.55-3.1l-31.17 10.39a11.95 11.95 0 0 0-8.17 11.34c0 4.53 2.56 8.66 6.61 10.69l11.08 5.54c9.41 4.71 19.79 7.16 30.31 7.16s22.59 27.29 32 32h66.75c8.49 0 16.62 3.37 22.63 9.37l13.69 13.69a30.503 30.503 0 0 1 8.93 21.57 46.536 46.536 0 0 1-13.72 32.98zM417 274.25c-5.79-1.45-10.84-5-14.15-9.97l-17.98-26.97a23.97 23.97 0 0 1 0-26.62l19.59-29.38c2.32-3.47 5.5-6.29 9.24-8.15l12.98-6.49C440.2 193.59 448 223.87 448 256c0 8.67-.74 17.16-1.82 25.54L417 274.25z"></path></svg>
+                            </a>
+                        </div><!-- end web -->
+
+                        <div class="w-8 h-8 bg-gray-800 rounded-full flex justify-center items-center"><!-- instagram -->
+                            <a href="#" class="hover:text-gray-400">
+                                <svg class="fill-current w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path></svg>
+                            </a>
+                        </div><!-- end instagram -->
+
+                        <div class="w-8 h-8 bg-gray-800 rounded-full flex justify-center items-center"><!-- twitter -->
+                            <a href="#" class="hover:text-gray-400">
+                                <svg class="fill-current w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path></svg>
+                            </a>
+                        </div><!-- end twitter -->
+
+                        <div class="w-8 h-8 bg-gray-800 rounded-full flex justify-center items-center"><!-- facebook -->
+                            <a href="#" class="hover:text-gray-400">
+                                <svg class="fill-current w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"></path></svg>
+                            </a>
+                        </div><!-- end facebook -->
+
+                    </div> <!-- end social -->
+                    <p class="mt-10">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil eligendi minima obcaecati illum veniam, cumque commodi aliquid deleniti adipisci ducimus libero alias, culpa vero voluptatem accusamus consequuntur fugiat ab? Delectus tempora cum illum illo, corrupti consequatur eos, numquam dolorem, nam blanditiis nesciunt? Consequatur, placeat voluptate? Quidem deleniti quam error quisquam distinctio officia nam asperiores assumenda quia? Eius non sit esse!
+                    </p>
+                    <div class="mt-12">
+                        <button class="flex bg-blue-500 text-white font-semibold px-4 py-4 hover:bg-blue-600 rounded transition ease-in-out duration-150">
+                            <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"></path><path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"></path></svg>
+                            <span class="ml-2">Play Trailer</span>
+                        </button>
+                    </div><!-- end trailer btn -->
+                </div> 
+            </div> <!-- end game data -->
+        </div><!-- end game details -->
+
+        <div class="image-container border-b border-gray-800 pb-12 mt-8">
+            <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Images</h2>
+            <div class="grid grid-cols-3 gap-12 mt-8 ">
+                @for($i = 1; $i <= 6; $i++)
+                    <div>
+                        <a href="#">
+                            <img class="hover:opacity-75 transition ease-in-out duration-150" src="/assets/screenshot.jpg" alt="screenshot">
+                        </a>
+                    </div>
+                @endfor
+            </div>
+        </div><!-- end images container -->
+
+        <div class="simular-games-container pb-12 mt-8">
+            <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Simular Games</h2>
+            <div class="popular-games text-sm grid grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 gap-12">
+                @for ($i = 1; $i <= 6; $i++)
+                    <div class="game mx-auto mt-8">
+                        <div class="relative inline-block">
+                            <a href="/game">
+                                <img class="w-48 hover:opacity-75 transition ease-in-out duration-150" src="/assets/resident.jpg" alt="">
+                            </a>
+                            <div class="absolute right-0 w-16 h-16 bg-gray-800 rounded-full" style="right:-20px; bottom: 60px">
+                                <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                    80%
+                                </div>
+                            </div>
+                            <a class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8" href="#">Resident Evil 3</a>
+                            <div class="text-gray-400 mt-1">PS4</div>
+                        </div>
+                    </div>
+                @endfor
+            </div>  
+        </div><!-- end simular games container -->
+
+    </div> <!-- end container -->
 @endsection
