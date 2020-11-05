2525c8fe27b7cb00d02072c7d6f24e5188813647
Karl Murray <karlm89@me.com>
2020-11-04 21:47:52 -0600

    finished ep6

---
+++ app/Http/Controllers/GamesController.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class GamesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+        $before = Carbon::now()->subMonths(12)->timestamp;
+        $current = Carbon::now()->timestamp;
+        $after = Carbon::now()->addMonths(12)->timestamp;
+
+        $popularGames = Http::withHeaders(config('services.igdb'))
+            ->withBody(
+                "
+                    fields name, cover.url, first_release_date, total_rating_count, platforms.abbreviation, rating, total_rating;
+
+                    where platforms = (45,48,49,130,165,163,6) 
+                        & (first_release_date >={$before} & first_release_date < {$after})
+                        & total_rating_count > 50
+                    ;
+
+                    sort total_rating_count desc;
+                    limit 12;
+                ",
+                "text/plain"
+            )->post('https://api.igdb.com/v4/games')->json();
+
+        $recentlyReviewed = Http::withHeaders(config('services.igdb'))
+            ->withBody(
+                "
+                    fields name, cover.url, first_release_date, total_rating_count, total_rating, platforms.abbreviation, rating, rating_count,summary;
+
+                    where platforms = (45,48,49,130,165,163,6) 
+                        & (first_release_date >={$before} & first_release_date < {$current})
+                        & rating_count > 5
+                    ;
+
+                    sort total_rating_count desc;
+                    limit 3;
+                ",
+                "text/plain"
+            )->post('https://api.igdb.com/v4/games')->json();
+
+        $mostAnticiapted = Http::withHeaders(config('services.igdb'))
+            ->withBody(
+                "
+                    fields name, cover.url, first_release_date, total_rating_count, total_rating, platforms.abbreviation, rating, rating_count, summary, hypes;
+
+                    where platforms = (45,48,49,130,165,163,6) 
+                        & (first_release_date >={$current})
+                        & hypes > 50
+                    ;
+
+                    sort hypes desc;
+                    limit 6;
+                ",
+                "text/plain"
+            )->post('https://api.igdb.com/v4/games')->json();
+
+        $comingSoon = Http::withHeaders(config('services.igdb'))
+            ->withBody(
+                "
+                    fields name, cover.url, first_release_date, total_rating_count, total_rating, platforms.abbreviation, rating, rating_count, summary, hypes;
+
+                    where platforms = (45,48,49,130,165,163,6) 
+                        & (first_release_date > {$current})
+                        & hypes > 50
+                    ;
+
+                    sort first_release_date asc;
+                    limit 3;
+                ",
+                "text/plain"
+            )->post('https://api.igdb.com/v4/games')->json();
+
+        return view('games.index')->with(compact('popularGames','recentlyReviewed', 'mostAnticiapted','comingSoon'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create() {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request) {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id) {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id) {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id) {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id) {
+        //
+    }
+}
--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -5,88 +5,100 @@
 @endsection
 
 @section('content')
+
     <div class="container mx-auto px-4">
         <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Popular Games</h2>
         <div class="popular-games text-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 gap-12 border-b border-gray-800 pb-16">
-            @for ($i = 1; $i <= 12; $i++)
+            @foreach ($popularGames as $game)
                 <div class="game mx-auto mt-8">
                     <div class="relative inline-block">
                         <a href="/game">
-                            <img class="w-48 hover:opacity-75 transition ease-in-out duration-150" src="/assets/resident.jpg" alt="">
+                            <img class="w-48 hover:opacity-75 transition ease-in-out duration-150" src="{{ Str::replaceFirst('thumb', 'cover_big',$game['cover']['url']) }}" alt="">
                         </a>
-                        <div class="absolute right-0 w-16 h-16 bg-gray-800 rounded-full" style="right:-20px; bottom: 60px">
+                        <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right:-20px; bottom: -20px">
                             <div class="font-semibold text-xs flex justify-center items-center h-full">
-                                80%
+                                {{ round($game['total_rating']) }}%
                             </div>
                         </div>
-                        <a class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8" href="#">Resident Evil 3</a>
-                        <div class="text-gray-400 mt-1">PS4</div>
+                    </div>
+                    <a class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8" href="#">{{ $game['name'] }}</a>
+                    <div class="text-gray-400 mt-1">
+                        @foreach($game['platforms'] as $platform)
+                            @if(array_key_exists('abbreviation', $platform))
+                                {{ $platform['abbreviation'] }}, 
+                            @endif
+                        @endforeach
                     </div>
                 </div>
-            @endfor
+            @endforeach
         </div> <!-- end popular games -->
-
         <div class="flex flex-col lg:flex-row my-10">
             <div class="recently-reviewed w-full lg:w-3/4 mr-0 lg:mr-32">
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently Reviewed</h2>
                 <div class="recently-reviewed-container space-y-12 mt-8">
-                    @for($i=1; $i <= 3; $i++)
+                    @foreach ($recentlyReviewed as $game)
                         <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
                             <div class="relative flex-none">
-                                <a href="/game">
-                                    <img class="w-48 hover:opacity-75 transition ease-in-out duration-150" src="/assets/resident.jpg" alt="">
+                                <a href="#">
+                                    <img src="{{ Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']) }}" alt="game cover" class="w-48 hover:opacity-75 transition ease-in-out duration-150">
                                 </a>
-                                <div class="absolute right-0 w-16 h-16 bg-gray-900 rounded-full" style="right:-20px; bottom: -20px">
+                                <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" style="right: -20px; bottom: -20px">
                                     <div class="font-semibold text-xs flex justify-center items-center h-full">
-                                        80%
+                                        {{ round($game['rating']).'%' }}
                                     </div>
                                 </div>
                             </div>
-                            <div class="ml-6">
-                                <a class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4" href="/game">Resident Evil 3</a>
-                                <div class="text-gray-400 mt-1">PS4</div>
-                                <p class="mt-4 text-gray-400 hidden lg:block">
-                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt cumque ipsum excepturi aperiam numquam similique, dicta ullam voluptas. Repellat vel eaque iste vero sequi quibusdam soluta, quo neque consequatur mollitia temporibus. Saepe, quam esse. Tempore dicta assumenda sit temporibus, corporis cum repudiandae.
+                            <div class="ml-6 lg:ml-12">
+                                <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">{{ $game['name'] }}</a>
+                                <div class="text-gray-400 mt-1">
+                                    @foreach ($game['platforms'] as $platform)
+                                        @if (array_key_exists('abbreviation', $platform))
+                                            {{ $platform['abbreviation'] }},
+                                        @endif
+                                    @endforeach
+                                </div>
+                                <p class="mt-6 text-gray-400 hidden lg:block">
+                                    {{ $game['summary'] }}
                                 </p>
                             </div>
-                        </div> <!-- end card -->
-                    @endfor
-                </div><!-- end recently review -->
+                        </div> <!-- end game -->
+                    @endforeach
+                </div>
             </div>
 
-            <aside class="right-sidebar flex flex-row lg:flex-col w-full items-end justify-between lg:w-1/4 mt-8 lg:mt-0 px-4 lg:mx-0 ">
-                
+            <aside class="right-sidebar flex flex-row flex-wrap lg:flex-col w-full lg:w-1/4 mt-8 lg:mt-0 px-4 lg:mx-0 ">
                 <div class="most-anticiapted-container space-y-10 mb-0 lg:mb-8">
                     <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Most Anticiapted</h2>
-                    @for($i=1; $i <= 4; $i++)
+                    @foreach($mostAnticiapted as $game)
                         <div class="game flex">
                             <a href="/game">
-                                <img class="w-16 hover:opacity-75 transition ease-in-out duration-150" src="/assets/resident.jpg" alt="">
+                                <img class="w-16 hover:opacity-75 transition ease-in-out duration-150" 
+                                src="{{ Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']) }}" alt="">
                             </a>
                             <div class="ml-4">
-                                <a class="text-sm font-semibold leading-tight hover:text-gray-400 mt-8" href="#">Resident Evil 3</a>
-                                <p class="text-sm text-gray-400 mt-1" href="#">Sept 16,2020</p>
+                                <a class="text-sm font-semibold leading-tight hover:text-gray-400 mt-8" href="#">{{ $game['name'] }}</a>
+                                <p class="text-sm text-gray-400 mt-1" href="#">{{ Carbon\Carbon::parse($game['first_release_date'])->format('M d,Y') }}</p>
                             </div>
-                                
                         </div> <!-- end game -->
-                    @endfor
+                    @endforeach
                 </div>
                 
-                <div class="most-anticiapted-container space-y-10">
+                <div class="most-anticiapted-container mt-8 space-y-10">
                     <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Coming Soon</h2>
-                    @for($i=1; $i <= 4; $i++)
-                        <div class="game mx-auto flex">
+                    @foreach($comingSoon as $game)
+                        <div class="game flex">
                             <a href="/game">
-                                <img class="w-16 hover:opacity-75 transition ease-in-out duration-150" src="/assets/resident.jpg" alt="">
+                                <img class="w-16 hover:opacity-75 transition ease-in-out duration-150" 
+                                src="{{ Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']) }}" alt="">
                             </a>
                             <div class="ml-4">
-                                <a class="text-sm font-semibold leading-tight hover:text-gray-400 mt-8" href="#">Resident Evil 3</a>
-                                <p class="text-sm text-gray-400 mt-1" href="#">Sept 16,2020</p>
+                                <a class="text-sm font-semibold leading-tight hover:text-gray-400 mt-8" href="#">{{ $game['name'] }}</a>
+                                <p class="text-sm text-gray-400 mt-1" href="#">{{ Carbon\Carbon::parse($game['first_release_date'])->format('M d,Y') }}</p>
                             </div>
                         </div> <!-- end game -->
-                    @endfor
+                    @endforeach
                 </div><!-- end coming soon -->
             </aside>
-        </div> <!-- end flex box -->
+        </div>
     </div> <!-- end container -->
 @endsection
